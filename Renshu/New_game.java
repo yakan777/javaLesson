1e4e695a24132396756190b3da5caa24fff226fd
yakan <yume_drop@hotmail.com>
2022-03-17 21:04:26 +0900

    練習1.1

--- Renshu/New_game.java
+++ Renshu/New_game.java
@@ -2,11 +2,12 @@ public class New_game{
 	public static void main(String[] args){
 		String name="ロト";
 		int hp=1000;
+		System.out.println(name+"HP"+"("+ hp +")");
 		System.out.println("スライムが出てきた！");
 		System.out.println("スライムの先制攻撃");
 		System.out.println(name + "は77のダメージを受けた");
 		hp = hp - 77;
-		System.out.println(name+"("+ hp +")");
+		System.out.println(name+"HP"+"("+ hp +")");
 
 
 	}
