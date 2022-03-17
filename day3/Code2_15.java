b15f1a3c42ea56fc373875cb69ba2bf2933eb5a1
yakan <yume_drop@hotmail.com>
2022-03-17 13:42:02 +0900

    ユーザーからの入力を受け取る

---
+++ day3/Code2_15.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Code2_15{
+	public static void main(String[] args){
+		System.out.print("あなたの名前を入力してください。");
+		String name=new Scanner(System.in).nextLine();
+		System.out.print("あなたの年齢を入力してください。");
+		int age = new Scanner(System.in).nextInt();
+		System.out.println("ようこそ、" + age + "歳の" + name + "さん");
+	}
+
+		
+}
+
