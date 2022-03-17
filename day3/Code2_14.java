b024c2e8e75f777a29b989faa2b68ca40668a1bd
yakan <yume_drop@hotmail.com>
2022-03-17 13:17:00 +0900

    乱数の生成

---
+++ day3/Code2_14.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Code2_14 {
+ public static void main(String[] args){
+ 	int r = new Random().nextInt(90);//0~89の90個
+	//nextInt(6)+5
+	//nextInt(大きい数-小さい数+1)+小さい数
+	System.out.println("あなたはたぶん、 "+ r +"歳ですね？");
+ }
+}
+
+
