2da6d3cef7cebab731e9b1c36f4eff5b8cd5ca80
yakan <yume_drop@hotmail.com>
2022-03-18 21:08:30 +0900

    練習3_18-1

--- Renshu/Age_demon.java
+++ Renshu/Age_demon.java
@@ -1,9 +1,7 @@
 public class Age_demon{
 	public static void main(String[] args){
-		int age;
-		int age2;
-		age = 10;
-		age2 =50;
+		int age = 10;
+		int age2 = 50;
 		System.out.println("我輩の年齢は"+ age +"万"+ age2+"歳"); 
 	}
 }
