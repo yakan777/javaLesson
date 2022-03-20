717ffe8a27b22d36b6ea499df14a38c816a5ae6e
yakan <yume_drop@hotmail.com>
2022-03-20 19:57:47 +0900

    練習3_20-3

---
+++ Renshu/Ransuu.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Ransuu{
+	public static void main(String[] args){
+		int age = new Random().nextInt(90);
+			age++;
+		int age2 = new Random().nextInt(90);
+			age++;
+			System.out.print("あなたの年齢は" + age + "歳ですよね…それとも" + age2 + "歳ですか？");
+	}
+}
