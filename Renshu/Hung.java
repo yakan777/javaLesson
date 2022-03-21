81bf09efc11b48c97734c1aeff95124759818d77
yakan <yume_drop@hotmail.com>
2022-03-21 21:19:09 +0900

    3_21

---
+++ Renshu/Hung.java
@@ -0,0 +1,16 @@
+public class Hung{
+	public static void main(String[] args){
+		int isHungry = 1;
+		String food = "おにぎり";
+		System.out.println("こんにちわ");
+		if(isHungry == 0){
+			System.out.println("お腹がいっぱいです");
+		}else{
+			System.out.println("腹ペコです");}
+		if(isHungry == 1){
+			System.out.println(food + "をいただきます");}
+
+			System.out.println("ごちそうさまでした");
+		}
+
+	}
