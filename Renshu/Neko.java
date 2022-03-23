efc927605bac2f8ce6a5d0654f07e79c4222de96
yakan <yume_drop@hotmail.com>
2022-03-23 18:46:22 +0900

    3_23

---
+++ Renshu/Neko.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Neko{
+	public static void main(String[] args){
+		System.out.println("この箱の中の動物は天気によって種類が変わり性別はランダムです。今の天気を確認してください。1 = 晴れ:2 =:曇り３=雨:");
+        int tenki = new Scanner(System.in).nextInt();
+	int onof =(int)(Math.floor(Math.random()*2) +1);
+	if(tenki == 1 && onof == 1){
+	   System.out.println("晴れているので箱の中にはオスのキリンがいます");
+       }else if(tenki == 1 && onof == 2){ 
+	   System.out.println("晴れているので箱の中にはメスのキリンがいます");
+       }else if(tenki == 2 && onof == 1){
+	   System.out.println("曇りなので箱の中にはオスの犬がいます");
+       }else if(tenki == 2 && onof == 2){
+	   System.out.println("曇りなので箱の中にはメスの犬がいます");
+       }else if(tenki == 3 && onof == 1){
+	   System.out.println("雨なので箱の中にはオスの猫がいます");
+       }else{ 
+	   System.out.println("雨りなので箱の中にはメスの猫がいます");
+       }
+
+	   
+	}
+}
