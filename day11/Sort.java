c4a8337841b225bd85f823b1e81ba13f5db29fc8
yakan <yume_drop@hotmail.com>
2022-03-31 14:54:23 +0900

    3_31

--- day11/Sort.java
+++ day11/Sort.java
@@ -1,15 +1,17 @@
-import.java.util*;
+import java.util.*;
 public class Sort{
 	public static void main(String[] args){
-		int[] data{3,1,5,2,7};
+		int[] data = {3,1,5,2,7};
 		System.out.println(Arrays.toString(data));
-    //単純ソート(昇順)
+		//単純ソート(昇順)
 		for(int i =0;i<data.length-1;i++){
 			for(int j =i+1;j<data.length;j++){
 				if(data[i]>data[j]){
 					int temp=data[i];
-				data[i]=data[j];
-				data[j]=temp;
+					data[i]=data[j];
+					data[j]=temp;
+				}
 			}
 		}
 	}
+}
