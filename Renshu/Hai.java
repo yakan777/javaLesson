ecf34b7ca9055d9650d541b9ae5c872e670204a5
yakan <yume_drop@hotmail.com>
2022-04-05 22:25:16 +0900

    4_5

---
+++ Renshu/Hai.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Arrays;
+public class Hai{
+	public static void main(String[] args){
+		System.out.println(">");
+		int[] dataA =new int[]{4,8,10};
+			for(int i =0;i<dataA.length;i++){
+				System.out.println(dataA[i]);
+			}
+	}
+}
