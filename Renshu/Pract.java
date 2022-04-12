efde207202b4934a4eeb91ba37d52fdb93af9279
yakan777 <yume_drop@hotmail.com>
2022-04-12 09:23:40 +0900

    4_12

---
+++ Renshu/Pract.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Pract{
+	public static void main(String[] args){
+		int n = result(3,5);
+		System.out.println(n);
+	}
+	public static int result(int a,int b){
+		int sum =0;
+		return sum+=a+b;
+	}
+}
