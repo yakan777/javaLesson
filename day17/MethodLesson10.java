51ea28a4f3ba48d0355e44931d79cb91bf1eaf76
yakan777 <yume_drop@hotmail.com>
2022-04-12 14:40:46 +0900

    day17追加

--- day17/MethodLesson10.java
+++ day17/MethodLesson10.java
@@ -4,9 +4,19 @@ public class MethodLesson10{
 		//値渡し
 		method1(n);
 		System.out.println(n);
+
+		int[] arr=new int[]{1,2,3};
+		method2(arr);
+		System.out.println(arr[0]);
 	}
 	static void method1(int n){
 		n++;
 		System.out.println(n);
+	}
+	//参照値渡し(メインメソッドにも変化が反映)
+	static void method2(int[] arr){
+		for(int i =0;i<arr.length;i++){
+			arr[i]=arr[i]+1;
+		}
 	}
 }
