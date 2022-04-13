ef9b2317a241cef5348ff2372127fd65c03c7339
yakan <yume_drop@hotmail.com>
2022-04-14 07:34:34 +0900

    4_13

--- Renshu/Pract4_13.java
+++ Renshu/Pract4_13.java
@@ -1,16 +1,18 @@
 import java.util.*;
 public class Pract4_13{
-	static int[] timesArray(int[] arr,int times){
-		int[] retArr = new int[arr.length];
-		for(int i =0;i<retArr.length;i++){
-			retArr[i] = arr[i]*times;
-		}
-		return retArr;
-	}
 	public static void main(String[] args){
-		int[] arr1 = new int[] {1,2,3};
-		int[] arr2 = timesArray(arr1,2);
-		System.out.println(Arrays.toString(arr1));
-		System.out.println(Arrays.toString(arr2));
+		hello();
+	}
+	static  void hello(){
+		int n = (int)(3*Math.random());{
+			if(n==0){
+				System.out.println("おはよう");
+			}else if(n==1){
+				System.out.println("こんにちは");
+			}else{
+				System.out.println("こんばんは");
+
+			}
+		}
 	}
 }
