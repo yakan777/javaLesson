abc07bc229c320e1ef336e05cf73bc29aa34270a
yakan <yume_drop@hotmail.com>
2022-04-17 21:54:15 +0900

    4_17

---
+++ Renshu/4_17/DogApp.java
@@ -0,0 +1,12 @@
+public class DogApp{
+	public static void main(String[] args){
+		Dog dog1 = new Dog();
+		dog1.name ="pig";
+		dog1.age ="3カ月";
+		dog1.name();
+		Dog dog2 = new Dog();
+		dog2.name ="tonkatsu";
+		dog2.age ="２週間前まで１歳";
+		dog2.name();
+	}
+}
---
+++ Renshu/4_17/DogApp2.java
@@ -0,0 +1,8 @@
+public class DogApp2{
+	public static void main(String[] args){
+		Dog dog2 = new Dog();
+		dog2.name ="tonkatsu";
+		dog2.age ="２週間前まで１歳";
+		dog2.name();
+	}
+}
