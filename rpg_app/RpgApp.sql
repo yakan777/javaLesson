6485c465ee13e46e86fc47072e68c60b12c759a1
yakan <yume_drop@hotmail.com>
2022-05-19 19:48:55 +0900

    5_19

--- rpg_app/RpgApp.sql
+++ rpg_app/RpgApp.sql
@@ -62,4 +62,81 @@ WHERE HP BETWEEN '120' AND '160'
 
 17
 SELECT 名称,職業コード FROM パーティー
-IN()
+WHERE 職業コード IN(01,10,11)
+
+18
+SELECT 名称,状態コード FROM パーティー
+WHERE 状態コード NOT IN('00','09')
+
+19
+SELECT * FROM パーティー
+WHERE HP > 100 AND MP > 100
+
+20
+SELECT * FROM パーティー
+WHERE ID LIKE 'A%' AND 職業コード LIKE '2%'
+
+21
+SELECT * FROM イベント
+WHERE タイプ ='03'
+AND 前提イベント番号 IS NOT NULL
+AND 後続イベント番号 IS NOT NULL
+
+22
+パーティ id
+イベント イベント番号
+
+23
+SELECT DISTINCT 状態コード FROM パーティー
+
+24
+SELECT ID,名称 FROM パーティー
+ORDER BY ID ASC
+
+25
+SELECT 職業コード,名称 FROM パーティー
+ORDER BY 名称 DESC
+
+26
+SELECT 名称,HP,状態コード FROM パーティー
+ORDER BY 状態コード ASC,HP DESC
+
+27
+SELECT タイプ,イベント番号,イベント名称,
+前提イベント番号,後続イベント番号 FROM イベント
+ORDER BY 1 ASC,2 ASC
+
+28
+SELECT * FROM パーティー
+ORDER BY HP DESC LIMIT 3
+
+29
+SELECT * FROM パーティー
+ORDER BY MP DESC LIMIT 1 OFFSET 2
+
+30
+SELECT
+CASE
+WHEN 職業コード LIKE '1%' THEN 'S'
+WHEN 職業コード LIKE '2%' THEN 'M'
+ELSE 'A'
+END AS 職業区分,職業コード,ID,名称
+FROM パーティー
+ORDER BY 職業コード
+
+31
+SELECT イベント番号 FROM イベント
+WHERE イベント番号 
+NOT IN(SELECT イベント番号 FROM 経験イベント)
+ORDER BY イベント番号
+
+32
+SELECT イベント番号 FROM イベント
+WHERE イベント番号
+IN(SELECT イベント番号 FROM 経験イベント WHERE クリア区分='1')
+AND タイプ = '2'
+
+33
+SELECT 名称 AS なまえ,HP AS 現在のHP,HP+50 AS 装備後のHP
+FROM パーティー
+WHERE 職業コード IN('11','21')
